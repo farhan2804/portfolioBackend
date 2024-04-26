34b35207f38ac296d57671745b4c22e4b6605ac1
farhan <el3317.farhanmahmood@gmail.com>
2024-04-27 04:01:57 +0530

    .

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
-    const connectionString = `mongodb://localhost:27017/portfoliodata`; // Local connection string
+    const connectionString = "mongodb://localhost:27017/portfoliodata"; // Local connection string
     await mongoose.connect(connectionString);
     console.log("Connection successful!");
   } catch (error) {
